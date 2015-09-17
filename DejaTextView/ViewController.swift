b30bcba2b994b59cb806e6a7cda2be79d156d119
Markus Schlegel <mail@markus-schlegel.com>
2015-09-17 15:57:53 +0200

    Refine appearance of sample app

--- DejaTextView/ViewController.swift
+++ DejaTextView/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textView: DejaTextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.textView.textContainerInset = UIEdgeInsets(top: 42, left: 16, bottom: 42, right: 16)
     }
 
     override func didReceiveMemoryWarning() {
