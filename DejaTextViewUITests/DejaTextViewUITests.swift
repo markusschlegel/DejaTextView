1cbf2f6e416e8ca40a3ec8bf6d4651c6346dff95
Markus Schlegel <mail@markus-schlegel.com>
2015-09-17 15:52:15 +0200

    Restrcture project, discard cocoapod layout

---
+++ DejaTextViewUITests/DejaTextViewUITests.swift
@@ -0,0 +1,35 @@
+//
+//  DejaTextViewUITests.swift
+//  DejaTextViewUITests
+//
+//  Created by Markus Schlegel on 17/09/15.
+//  Copyright © 2015 Markus Schlegel. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class DejaTextViewUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
